bff8599bba211c3bcc35d335b3511902d3cec649
arcasoy <arcasoy1@gmail.com>
2022-08-15 00:39:57 -0400

    Repaired dynamic rendering of navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,11 +22,11 @@ const StyledNavbar = styled.div`
   box-shadow: 0px 5px 20px 10px rgba(0, 0, 0, 0.7);
 
   #nav-flex-elements {
-    display: flex;
+    display: inline-flex;
     flex-wrap: nowrap;
     flex-direction: row;
+    flex: 0 0;
     margin: 0 auto;
-    max-width: 75%;
     min-height: 50px;
     justify-content: center;
     align-items: center;
@@ -65,13 +65,15 @@ const StyledNavbar = styled.div`
     bottom: 0;
   }
 
-  @media all and (max-width: 1000px) {
-    #nav-logo-link {
-      margin-left: 0px;
-      left: 0px;
+  @media all and (max-width: 1112px) {
+    #nav-flex-elements {
+      flex-direction: column;
+    }
+    #nav-links {
+      flex-direction: column;
     }
     #nav-links > a {
-      display: none;
+      padding: 0.5em;
     }
     #nav-balance-logo {
       display: none;
@@ -79,15 +81,9 @@ const StyledNavbar = styled.div`
   }
 `;
 
-// TODO: Change styles from React styled-components to React inline CSS referencing variables
-
-const someBooleanSomewhere = false;
-
-const navFlexStyle = {
-  backgroundColor: someBooleanSomewhere ? "red" : "blue",
-};
-
 export default function Navbar() {
+  const scrollDuration = 2;
+
   const [dynamicOffset, setDynamicOffset] = useState(5);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -107,7 +103,7 @@ export default function Navbar() {
   return (
     // TODO: Replace scrolling Link with window.scrollTo and scroll-behavior: smooth
     <StyledNavbar id="main-nav">
-      <div id="nav-flex-elements" style={navFlexStyle}>
+      <div id="nav-flex-elements">
         <a href="/" id="nav-logo-link">
           <img
             src={logo}
@@ -119,19 +115,21 @@ export default function Navbar() {
           <Link
             activeClass="active"
             to="App"
-            onClick={handleOnClick}
+            //onClick={handleOnClick}
             offset={dynamicOffset}
             spy={true}
             smooth={true}
+            duration={scrollDuration}
           >
             Home
           </Link>
           <Link
             to="resumePanel"
-            onClick={handleOnClick}
+            //={handleOnClick}
             offset={dynamicOffset}
             spy={true}
             smooth={true}
+            duration={scrollDuration}
           >
             Resume
           </Link>
@@ -141,15 +139,17 @@ export default function Navbar() {
             offset={dynamicOffset}
             spy={true}
             smooth={true}
+            duration={scrollDuration}
           >
             Portfolio
           </Link>
           <Link
             to="contactPanel"
-            onClick={handleOnClick}
+            //onClick={handleOnClick}
             offset={dynamicOffset}
             spy={true}
             smooth={true}
+            duration={scrollDuration}
           >
             Contact
           </Link>
@@ -159,6 +159,7 @@ export default function Navbar() {
             offset={dynamicOffset}
             spy={true}
             smooth={true}
+            duration={scrollDuration}
           >
             Stay Updated
           </Link>
