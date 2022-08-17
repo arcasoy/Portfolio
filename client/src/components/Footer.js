f5d862ba8dfb4d32d89f81131a61d531f502cef3
arcasoy <arcasoy1@gmail.com>
2022-08-17 00:50:52 -0400

    Adds & styles footer elements

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,24 +1,61 @@
 //Import packages
 import React from "react";
+import { IconContext } from "react-icons";
 
 //Import components
 import HomePanel from "./HomePanel";
 
 //Import assets
 import logo from "../assets/images/AA.png";
-import { GoMail } from "react-icons/go";
 import { RiLinkedinFill } from "react-icons/ri";
 import { RiGithubFill } from "react-icons/ri";
 import { RiTwitterFill } from "react-icons/ri";
+import { RiInstagramLine } from "react-icons/ri";
 
 export default function Footer() {
   return (
     <HomePanel>
-      <img src={logo}></img>
-      <RiGithubFill className="footer-icon" />
-      <RiLinkedinFill className="footer-icon" />
-      <GoMail className="footer-icon" />
-      <RiTwitterFill className="footer-icon" />
+      <img
+        src={logo}
+        alt="Alex's logo, two A's turned sideways similar to HTML tags"
+        style={{
+          background: "white",
+          borderRadius: "100%",
+          height: "3em",
+          padding: "none",
+          margin: "1em",
+        }}
+      ></img>
+      <IconContext.Provider
+        value={{
+          color: "white",
+          size: "1.5em",
+          className: "footer-icon",
+        }}
+      >
+        <div
+          id="footer-icons"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "1em",
+          }}
+        >
+          <a href="https://github.com/arcasoy">
+            <RiGithubFill className="footer-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/alexander-arcasoy/">
+            <RiLinkedinFill className="footer-icon" />
+          </a>
+          <a href="https://twitter.com/arcasoi">
+            <RiTwitterFill className="footer-icon" />
+          </a>
+          <a href="https://www.instagram.com/arcasoi/">
+            <RiInstagramLine className="footer-icon" />
+          </a>
+        </div>
+      </IconContext.Provider>
+      <p>© 2019-2022 by Alexander Arcasoy. All rights reserved.</p>
     </HomePanel>
   );
 }
