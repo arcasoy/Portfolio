60f82affec5645c3f46f04a5f32ba5fdb6c58637
arcasoy <arcasoy1@gmail.com>
2022-10-02 14:57:39 -0400

    Add first content to portfolio site page

--- client/src/pages/PortfolioSitePage.js
+++ client/src/pages/PortfolioSitePage.js
@@ -4,13 +4,10 @@ import React, { useEffect } from "react";
 // Import components
 import PortPagePanel from "../components/PortPagePanel";
 
-// Import assets (old)
-import SampleAutomation from "../assets/images/portfolioItems/BVE/Assembly-Machine.png";
-import PackMLStates from "../assets/images/portfolioItems/BVE/PACKML Chart Updated 2019.png";
-import Jira from "../assets/images/portfolioItems/BVE/Screen-kanban board.png";
-
 // Import assets
 import AALogo from "../assets/images/portfolioItems/PortfolioSite/AA.png";
+import PortProjectsJSON from "../assets/images/portfolioItems/PortfolioSite/PortProjectsJSON.PNG";
+import ContactFormAWS from "../assets/images/portfolioItems/PortfolioSite/ContactFormAWS.PNG";
 
 export default function BVEPage() {
   useEffect(() => {
@@ -69,25 +66,120 @@ export default function BVEPage() {
       <PortPagePanel>
         <img className="image-panel-img" src={AALogo} alt="Alex's Logo"></img>
       </PortPagePanel>
-      {/* Front End panel */}
+      {/* Purpose panel */}
       <PortPagePanel mode="off-white">
+        <h3 className="port-page-section-title">Purpose</h3>
+        <div className="port-page-section-content-offset">
+          <p>
+            My portfolio, the website you're perusing now, was made with the
+            intention of sharing my work, projects, and interests with anyone
+            interested. Prior to building this site myself, I paid (more than
+            I'd like to admit) for a Wix subscription annually.
+            <br /> <br />
+            To reduce design time, I challenged myself to mimic the design I
+            created for my{" "}
+            <a href="https://arcasoy1.wixsite.com/portfolio">Wix site</a>, which
+            is still hosted (for free) for comparison.
+            <br /> <br />
+            Building this site was an opportunity to show off my web development
+            skills, build features to my liking rather than Wix's templates, and
+            reduce the annual cost of this website tenfold.
+          </p>
+        </div>
+      </PortPagePanel>
+      {/* Front End panel */}
+      <PortPagePanel>
         <h3 className="port-page-section-title">Front End</h3>
         <div className="port-page-section-content-offset">
-          <p>Coming soon...</p>
+          <p>
+            The frontend was build with React. I began with create-react-app,
+            then added my own folder structure after researching optimal React
+            folder structure for medium-sized projects, including folders for
+            assets, components, context, hooks, and pages.
+            <br /> <br />
+            Below are some of the highlights that may not be noticable from a
+            glance at the site. I'm always looking to impove, so if you see any
+            pitfalls on the site please feel free to reach out using the contact
+            form on the home page to get in touch!
+          </p>
+        </div>
+        <div className="port-page-section-content-center">
+          <h5>Portfolio Cards</h5>
+          <p>
+            The portfolio cards from the home page, like many other parts of the
+            website, were created as components. These were slightly different
+            though as I wanted to showcase displaying content dynamically based
+            on data. Rather than calling an API on each render for the portfolio
+            card data, I added a JSON file to the data folder that drives all of
+            the content that is loaded into each portfolio card, even organizing
+            them into the software and hardware category.
+          </p>
+
+          <img
+            src={PortProjectsJSON}
+            alt="Data format for portfolio card components"
+          ></img>
+          <h5>Responsiveness</h5>
+          <p>
+            This site was designed to be responsive down to screens 350px wide.
+            Changes in layout in portfolio pages and the navigation bar occur as
+            the size of the window is reduced. Flex directions change from row
+            to column, the nagivation bar hides elements and adds a hamburger
+            menu, and some fonts scale with viewport change.
+          </p>
+          <h5>Packages</h5>
+          <p>
+            Getting this site up and running wouldn't have gone so smoothly
+            without the help of some React packages. Of course, react-router-dom
+            was a must to handle routing between pages. Additionally,
+            react-modal was very useful in getting the Stay Updated modal set up
+            quickly. Finally, I dipped my toes into the styled-components
+            package which allowed for styling to live inside each component
+            declaration rather than in a global style sheet. This was certainly
+            effective for components like buttons and hamburger drop down, but
+            was cumbersome with larger components like the navigation bar.
+          </p>
         </div>
       </PortPagePanel>
-      {/* Back End panel */}
-      <PortPagePanel>
-        <h3 className="port-page-section-title">Back End</h3>
-        <div className="port-page-section-content-offset">
-          <p>Coming soon...</p>
-        </div>
-      </PortPagePanel>
-      {/* Hosting panel */}
+      {/* Back End & Hosting panel */}
       <PortPagePanel mode="off-white">
-        <h3 className="port-page-section-title">Hosting</h3>
+        <h3 className="port-page-section-title">Back End & Hosting</h3>
         <div className="port-page-section-content-offset">
-          <p>Coming soon...</p>
+          <p>
+            The elements of the site that require backend services are the
+            contact form and the stay updated sign up. Considering this project
+            had very few backend services required, I decided to utilize FaaS
+            services. To gain more experience with AWS, services I had yet to
+            use, I decided to try their suite of tools when building the
+            backend.
+          </p>
+        </div>
+        <div className="port-page-section-content-center">
+          <h5>Hosting</h5>
+          <p>
+            This site was built using AWS Amplify. I set up continuous
+            deployment (CD) from the main branch of my GitHub repository and
+            pointed my domain to the amplify app by changing the CNAME on my
+            site's DNS records through Wix as I purchased the domain through
+            their platform.
+          </p>
+          <h5>Contact Form</h5>
+          <p>
+            The contact form on the home page uses AWS's API Gateway, Lambda,
+            and Simple Email Service (SES) to email me when a contact form is
+            submitted. The form element calls the API endpoint set up on API
+            Gateway, which triggers a Lambda function to format the data and
+            fire an email using SES from mail@alexarcasoy.com
+          </p>
+          <img
+            src={ContactFormAWS}
+            alt="AWS Architecture for Contact Form"
+          ></img>
+          <h5>Stay Updated</h5>
+          <p>
+            Coming soon... implementing this with AWS Simple Notification System
+            (SNS).
+          </p>
         </div>
       </PortPagePanel>
     </>
